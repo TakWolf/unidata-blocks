46bd52863c1f57da1cc616475f522b49042243ec
TakWolf <takwolf@foxmail.com>
2023-05-09 18:42:48 +0800

    i18n 文件更新工具

---
+++ tools/update_i18n_files.py
@@ -0,0 +1,38 @@
+import os
+import re
+
+import unidata_blocks
+from tools import i18n_dir
+
+
+def load_localized_names(i18n_file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes]):
+    localized_names = {}
+    with open(i18n_file_path, 'r', encoding='utf-8') as file:
+        for line in file.readlines():
+            line = line.split('#', 1)[0].strip()
+            if line == '':
+                continue
+            tokens = re.split(r':\s', line)
+            if len(tokens) >= 2:
+                localized_names[tokens[0]] = tokens[1]
+    return localized_names
+
+
+def main():
+    for i18n_file_name in os.listdir(i18n_dir):
+        if not i18n_file_name.endswith('.txt'):
+            continue
+        i18n_file_path = os.path.join(i18n_dir, i18n_file_name)
+        localized_names = load_localized_names(i18n_file_path)
+
+        with open(i18n_file_path, 'w', encoding='utf-8') as file:
+            file.write(f'# {i18n_file_name}\n\n')
+            for block in unidata_blocks.get_blocks():
+                if block.name in localized_names:
+                    file.write(f'{block.name}: {localized_names[block.name]}\n')
+                else:
+                    file.write(f'{block.name}:\n')
+
+
+if __name__ == '__main__':
+    main()
