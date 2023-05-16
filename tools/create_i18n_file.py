514cd1e9677b52e28053ad5e764d0c46a1ce9a38
TakWolf <takwolf@foxmail.com>
2023-05-16 15:52:29 +0800

    优化 i18n 模版创建脚本

--- tools/create_i18n_file.py
+++ tools/create_i18n_file.py
@@ -8,9 +8,10 @@ def main(locale: str):
     if not os.path.exists(i18n_dir):
         os.makedirs(i18n_dir)
 
-    i18n_file_path = os.path.join(i18n_dir, f'{locale}.txt')
+    i18n_file_name = f'{locale}.txt'
+    i18n_file_path = os.path.join(i18n_dir, i18n_file_name)
     with open(i18n_file_path, 'w', encoding='utf-8') as file:
-        file.write(f'# {locale}.txt\n\n')
+        file.write(f'# {i18n_file_name}\n\n')
         for block in unidata_blocks.get_blocks():
             file.write(f'{block.name}:\n')
 
