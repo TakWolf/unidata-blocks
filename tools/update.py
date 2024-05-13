bfdeaeeb336685b58add48cfbae0169a62364c5e
TakWolf <takwolf@foxmail.com>
2024-05-13 11:22:51 +0800

    代码整理

--- tools/update.py
+++ tools/update.py
@@ -10,8 +10,7 @@ blocks_doc_url = 'https://www.unicode.org/Public/UNIDATA/Blocks.txt'
 
 project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 unidata_dir = os.path.join(project_root_dir, 'src', 'unidata_blocks', 'unidata')
-blocks_file_name = 'Blocks.txt'
-blocks_file_path = os.path.join(unidata_dir, blocks_file_name)
+blocks_file_path = os.path.join(unidata_dir, 'Blocks.txt')
 translations_dir = os.path.join(unidata_dir, 'translations')
 translations_tmp_dir = os.path.join(project_root_dir, 'build', 'translations')
 languages_file_path = os.path.join(unidata_dir, 'languages.txt')
@@ -23,6 +22,7 @@ def main():
     assert 'text/plain' in response.headers['Content-Type']
     with open(blocks_file_path, 'w', encoding='utf-8') as file:
         file.write(response.text)
+    # noinspection PyProtectedMember
     unicode_version, blocks = unidata_blocks._parse_blocks(response.text)
 
     if os.path.exists(translations_tmp_dir):
@@ -31,18 +31,19 @@ def main():
 
     languages = ['en']
 
-    for translation_file_name in os.listdir(translations_dir):
-        if not translation_file_name.endswith('.txt'):
+    for file_name in os.listdir(translations_dir):
+        if not file_name.endswith('.txt'):
             continue
-        language = langcodes.standardize_tag(translation_file_name.removesuffix('.txt'))
+        language = langcodes.standardize_tag(file_name.removesuffix('.txt'))
         languages.append(language)
 
-        translation_file_path = os.path.join(translations_dir, translation_file_name)
-        with open(translation_file_path, 'r', encoding='utf-8') as file:
+        file_path = os.path.join(translations_dir, file_name)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            # noinspection PyProtectedMember
             translation = unidata_blocks._parse_translation(file.read())
 
-        translation_tmp_file_path = os.path.join(translations_tmp_dir, translation_file_name)
-        with open(translation_tmp_file_path, 'w', encoding='utf-8') as file:
+        tmp_file_path = os.path.join(translations_tmp_dir, file_name)
+        with open(tmp_file_path, 'w', encoding='utf-8') as file:
             file.write(f'# Unicode: {unicode_version}\n')
             file.write(f'# {language}\n\n')
             for block in blocks:
